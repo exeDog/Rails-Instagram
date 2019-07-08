02e4727c945e9b387c48d40870cc0a7c6283e08a
priyank rege <priyankrege@gmail.com>
2019-07-08 00:15:12 -0500

    Twitter authentication and login.

--- Gemfile
+++ Gemfile
@@ -66,6 +66,15 @@ gem 'wdm', '>= 0.1.0' if Gem.win_platform?
 #bootstrap
 gem 'bootstrap', '~>4.0.0'
 gem 'jquery-rails'
+
+# Toastr for flash
 gem 'toastr-rails'
+
+#OmniAuth twitter
+gem 'omniauth','~> 1.6'
+gem 'omniauth-twitter'
+
+
+
 
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,4 +296,6 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  config.omniauth :twitter, "pQwTtkrELhlrfcU9Eq5wNEkuH", "qvy6Q7x3Ke1pWMAeeA9aujWCcWLP92O0ODOknWqu3ywO0Sj5gP"
 end
