6685528c762b6d7286f830824ac39fb4eace58f0
priyank rege <priyankrege@gmail.com>
2019-07-15 23:42:49 -0500

    Profile page routing and styling.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
              path: '',
              path_names: {sign_in: "login", sign_out: 'logout', edit: 'profile',
                           sign_up: 'registration'},
-             controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+             controllers: {omniauth_callbacks: 'omniauth_callbacks',
+                           registrations: 'registrations'}
 
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
